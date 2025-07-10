ad08d629b2a8a3d546c8101fc5cb19e0eec2e47a
Farah-Stu <selma.salam45@gmail.com>
2025-07-10 23:57:21 +0100

    fixing the median code

--- Sprint-1/fix/median.js
+++ Sprint-1/fix/median.js
@@ -5,10 +5,33 @@
 // Hint: Please consider scenarios when 'list' doesn't have numbers (the function is expected to return null)
 // or 'list' has mixed values (the function is expected to sort only numbers).
 
+//function calculateMedian(list) {
+// const middleIndex = Math.floor(list.length / 2);
+//const median = list.splice(middleIndex, 1)[0];
+//return median;
+
+//module.exports = calculateMedian;
+
 function calculateMedian(list) {
-  const middleIndex = Math.floor(list.length / 2);
-  const median = list.splice(middleIndex, 1)[0];
-  return median;
+  if (!Array.isArray(list)) return null;
+
+  let numbers = [];
+  for (let item of list) {
+    if (typeof item === "number") {
+      numbers.push(item);
+    }
+  }
+  if (numbers.length === 0) {
+    return null;
+  }
+  numbers.sort((a, b) => a - b);
+
+  let middle = Math.floor(numbers.length / 2);
+  if (numbers.length % 2 === 0) {
+    return (numbers[middle - 1] + numbers[middle]) / 2;
+  } else {
+    return numbers[middle];
+  }
 }
 
 module.exports = calculateMedian;
