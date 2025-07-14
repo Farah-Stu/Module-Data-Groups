ecd310ce48eae67be85a989caffc9b8fa155b22e
Farah-Stu <selma.salam45@gmail.com>
2025-07-15 00:30:43 +0100

    updating sum file

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -1,4 +1,9 @@
-function sum(elements) {
+function sum(arr) {
+    return arr
+        .filter(item=> typeof item === 'number') // Filter out non-numeric values
+        .reduce((total, num) => total + num, 0); // Sum the numeric values
+    
+    
 }
 
 module.exports = sum;
